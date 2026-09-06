4365fbeb3391dc5f40b54caa3af3d7f878b53325
vsp097 <v.sarmientop01@gmail.com>
2026-09-06 00:42:54 -0400

    Prepare for deployment

--- database.py
+++ database.py
@@ -7,6 +7,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(
     DATABASE_URL,
     connect_args={"client_encoding": "utf8"}
